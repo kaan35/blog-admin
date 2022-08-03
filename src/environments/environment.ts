d19deefa4e0cacb7d7efd9df831322eadf96f1c1
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-08-03 16:18:03 +0300

    chore: added apiUrl key to environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'API_URL',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
